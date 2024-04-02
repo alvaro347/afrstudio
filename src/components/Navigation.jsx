ad5648c7a13432978e89097015318dc02ea94f3e
Alvaro <alvaro347@gmail.com>
2024-04-02 14:20:30 +0100

    Card and navigation works

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../css/nav.css"
+import logo from "../img/Logo.png";
 
 export default function Navigation() {
 	return (
 		<nav>
-			<a href="#projects" className="nav-link">Projects</a>
-			<a href="#about" className="nav-link">About</a>
-			<a href="#contact" className="nav-link">Contact</a>
+			<div className="logo">
+			<a href="#header"><img src={logo} alt="logo" /></a>
+			</div>
+			<div>
+				<a href="#projects" className="nav-link">Projects</a>
+				<a href="#about" className="nav-link">About</a>
+				<a href="#contact" className="nav-link">Contact</a>
+			</div>
 		</nav>
 	);
 }
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import '../css/ProjectCard.css';
-import projectExample from '../img/code.png'
-import logo from '../img/github.svg'
+import "../css/ProjectCard.css";
+import projectExample from "../img/code.png";
+import logo from "../img/github.svg";
 
 function ProjectCard() {
 	return (
 		<div className="card">
 			<img src={projectExample} alt="Card" className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
 			<div className="card-content">
-				<h2 class="card-title">Title</h2>
-				<p className="card-description">This is a small description of the card content.</p>
-				<div className="icons flex">
-					<img src={logo} alt="Icon 1" className="" />
-					<img src={logo} alt="Icon 2" />
+				<h2 class="card-title">Project Title</h2>
+				<p className="card-description">This is a small description of the card content. This projects it's about this and that. the content an intention was to showcase react skills combined with fetching data.</p>
+				<div className="more-info flex justify-between">
+					<div className="icons flex">
+						<img src={logo} alt="Icon 1" className="" />
+						<img src={logo} alt="Icon 2" />
+					</div>
+					<div className="self-center more-info-text grid">
+						<p className="">More information &#10230;</p>
+					</div>
 				</div>
 			</div>
 		</div>
