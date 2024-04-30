efcafbd6438e680d7e01c7c535d9fa402fab9089
Alvaro <alvaro347@gmail.com>
2024-04-30 11:36:18 +0100

    Footer starting point

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,22 @@ import "../css/Footer.scss";
 function Footer() {
   return (
     <footer id="footer">
-
       <div className="footer-content">
-   
+        <div className="footer-contact">
+          <h3 className="footer-title">Contact Information</h3>
+          <button className="footer-button">Email</button>
+          <button className="footer-button">Telegram</button>
+        </div>
+        <div className="footer-list-links">
+
         <div className="footer-links">
+          <h3 className="footer-title">Content</h3>
           <ul className="social-list">
             <li className="social-icon">
               <p>Projects</p>
+            </li>
+            <li className="social-icon">
+              <p>Skills</p>
             </li>
             <li className="social-icon">
               <p>About</p>
@@ -21,29 +30,30 @@ function Footer() {
           </ul>
         </div>
 
-        <div className="footer-social">
+        <div className="footer-links">
+          <h3 className="footer-title">Content</h3>
           <ul className="social-list">
             <li className="social-icon">
               <a href="https://www.youtube.com/channel/UCQ79F_bjrqNf8wKI5LVhFBA?view_as=subscriber">
                 <i className="fab fa-youtube footer-icon"></i>
-                <p>YouTube</p>
+                <p>GitHub</p>
               </a>
             </li>
             <li className="social-icon">
               <a href="https://www.instagram.com/alvarogna/">
-                <i className="fab fa-instagram footer-icon"></i>
-                <p>YouTube</p>
+                <i className="footer-icon"></i>
+                <p>LinkedIn</p>
               </a>
             </li>
             <li className="social-icon">
               <a href="https://alvaro347.deviantart.com">
-                <i className="fab fa-deviantart footer-icon"></i>
-                <p>YouTube</p>
+                <i className="footer-icon"></i>
+                <p>SketchFab</p>
               </a>
             </li>
             <li className="social-icon">
               <a href="https://www.linkedin.com/in/alvaro-fernandez-rodriguez/">
-                <i className="fab fa-linkedin footer-icon"></i>
+                <i className="footer-icon"></i>
                 <p>YouTube</p>
               </a>
             </li>
@@ -55,14 +65,20 @@ function Footer() {
             </li>
           </ul>
         </div>
-        
+        </div>
       </div>
       <div className="footer-logo">
-          <h2 className="footer-title">AFR-STUDIO</h2>
+        <h2 className="footer-title-logo">AFR-STUDIO</h2>
+        <div className="footer-bottom">
+          
           <a href="/" className="footer-text">
-            &copy; 2024 Alvaro Fernandez (AFR)
+            &copy; 2024 AFR Studio
+          </a>
+          <a href="/" className="footer-text">
+            Site
           </a>
         </div>
+      </div>
     </footer>
   );
 }
