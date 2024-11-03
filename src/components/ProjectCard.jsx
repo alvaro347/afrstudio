f96ea48c0ec8477868fe3fab5f7209c034436021
Alvaro <alvaro347@gmail.com>
2024-11-03 19:00:01 +0000

    Clean file and structure

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../css/ProjectCard.scss";
 // import ProjectCardIcons from "./ProjectCardIcons";
 import ProjectPopup from "./ProjectPopup";
